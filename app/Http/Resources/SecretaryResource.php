6351d3be2860a0e6c20aef54ce9d31da76b40532
ali <aliboune184@gmail.com>
2026-06-04 12:31:55 +0300

    feat(secretary): add SecretaryController, SecretaryRequest, SecretaryResource, and SecretaryService for managing secretaries

---
+++ app/Http/Resources/SecretaryResource.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SecretaryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        $data = [];
+
+        $this->whenLoaded('user', function () use (&$data) {
+            $data['name'] = $this->user->fname . ' ' . $this->user->lname;
+            $data['phone'] = $this->user->phone;
+            $data['dob'] = Carbon::parse($this->user->dob)->format('Y-m-d');
+            $data['gender'] = $this->user->gender;
+        });
+
+        $this->whenLoaded('room', function () use (&$data) {
+            $data['room'] = $this->room->name;
+        });
+
+        // $this->whenLoaded('clinic', function () use (&$data) {
+        //     $data['clinic'] = $this->clinic->name;
+        // });
+
+        return [
+            'id' => $this->id,
+            'user_id' => $this->user_id,
+            'room_id' => $this->room_id,
+            'clinic_id' => $this->clinic_id,
+            'created_at' => $this->created_at->format('Y-m-d'),
+            ...$data,
+        ];
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,17 @@
 
 namespace App\Providers;
 
+use App\Models\Secretary;
+use App\Policies\ViewSecretaryPolicy;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    // protected array $policies = [
+    //     Secretary::class => ViewSecretaryPolicy::class,
+    // ];
     /**
      * Register any application services.
      */
@@ -19,6 +26,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Gate::policy(Secretary::class, ViewSecretaryPolicy::class);
+
+        // Define the 'viewSecretary' gate for view secretary
+        Gate::define('viewSecretary', function ($user, $user_room_id, $secretary_room_id) {
+            return Auth::user()->hasRole('owner')
+                ||
+                ($user->can("view room {$user_room_id}") && $secretary_room_id == $user_room_id);
+        });
     }
 }
