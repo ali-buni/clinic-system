a33211699d34b2a3ba5c3e93bc2c296e6547b808
ali <aliboune184@gamil.com>
2026-08-07 14:46:04 +0300

    feat: add authentication and throttling middleware to disease, location, medicine, and schedule routes

--- routes/v1/location.php
+++ routes/v1/location.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\LocationApiController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/countries', [LocationApiController::class, 'countries']);
-Route::get('/countries/{countryCode}/governorates', [LocationApiController::class, 'governorates']);
-Route::get('/countries/{countryCode}/governorates/{governorateCode}/cities', [LocationApiController::class, 'cities']);
+Route::middleware(['auth:sanctum', 'throttle:100,1'])->group(function () {
+    Route::get('/countries', [LocationApiController::class, 'countries']);
+    Route::get('/countries/{countryCode}/governorates', [LocationApiController::class, 'governorates']);
+    Route::get('/countries/{countryCode}/governorates/{governorateCode}/cities', [LocationApiController::class, 'cities']);
+});
--- routes/v1/medicines.php
+++ routes/v1/medicines.php
@@ -4,7 +4,8 @@ use App\Http\Controllers\Medicine\MedicineController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('medicines')->controller(MedicineController::class)->group(function () {
-    Route::get('search', 'searchMedicine');
+    Route::get('search', 'searchMedicine')
+        ->middleware(['auth:sanctum', 'throttle:100,1']);
     Route::post('/', 'store')
         ->middleware(['auth:sanctum', 'throttle:100,1', 'checkaccess:role:doctor,secretary,owner', 'checkaccess:permission:manage m/d']);
 });
